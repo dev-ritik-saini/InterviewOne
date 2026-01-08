87c0fd12a41f46ff0f9da8073640e13c824af0b8
Ritik Saini(dev-rishiz) <ritikksainii26@gmail.com>
2026-01-08 19:20:33 +0530

    Database setup and running check.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
-import "./App.css";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
-      <h1>BAllllwlwlwlwlwlwlwl</h1>
+      <h1>Welcome to InterviewOne</h1>
+
+      <SignedOut>
+        <SignInButton mode="modal"></SignInButton>
+        <SignUpButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <UserButton />
     </>
   );
 }
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,17 @@
-import express from "express"
+import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 
 app.get("/", (req, res) => { res.status(200).json({ msg: "Server is sending api response." }) });
 
-app.listen(ENV.PORT, () =>
-    console.log("Server running is on port  ")
-) 
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () =>
+            console.log("Server running is on port:", ENV.PORT))
+    } catch (error) {
+        console.log("Error starting server:", error);
+    }
+}
+startServer();
